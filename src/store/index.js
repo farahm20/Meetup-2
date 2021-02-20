c23ad6bd5f6cb4ecf77d42b1ce75c852f68105fd
farahm20 <mahboob.farah@gmail.com>
2021-02-20 23:42:37 +0100

    Trying to run heroku

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+// import ax from "axios";
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    apiUrl: "https://api.jsonbin.io/b/602e519e0665b21b00b96d0d/1",
+    apiKey: "$2b$10$jLA6wytLYVhKQGfD3ITFvuL/QNpWWaNMUAgAAwic4GMEK9KMqFzOu",
+
   },
   mutations: {
   },
